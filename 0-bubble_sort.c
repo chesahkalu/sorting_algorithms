e0d687fa1a9b142c2a77620d8b20d440320888d7
Chesachi Victor Kalu <chesahkalu@gmail.com>
2023-01-24 15:10:20 -0800

    task 1 efficiency

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,29 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of integers in ascending order using the
- * Bubble sort algorithm
- * @array: The array to be sorted
- * @size: The size of the array
- *
- * Return: void
+ *bubble_sort - sorts an array of integers in ascending orde
+ *@array: the array to be sort
+ *@size: the size of the array
  */
+
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, n, new_n;
-	int swap;
+	size_t j, i;
+	int n;
 
-	if (array == NULL || size < 2)
+	if (size < 2)
 		return;
-
-	n = size;
-	while (n > 0)
+	for (i = 0; i < size - 1; i++)
 	{
-		new_n = 0;
-		for (i = 0; i < n - 1; i++)
+		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[i + 1])
+			if (array[j] > array[j + 1])
 			{
-				swap = array[i];
-				array[i] = array[i + 1];
-				array[i + 1] = swap;
-				new_n = i + 1;
+				n = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = n;
 				print_array(array, size);
 			}
 		}
-		n = new_n;
 	}
 }
